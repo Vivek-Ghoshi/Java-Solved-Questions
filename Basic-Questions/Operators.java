23528d3b7d4560da26bf745f51c6bb994769e898
Vivek ghoshi <the.vivekghoshi@gmail.com>
2025-01-06 11:08:39 +0530

    sum of two numbers

---
+++ Basic-Questions/Operators.java
@@ -0,0 +1,7 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 5 , b = 6 ;
+        int c = a + b ;
+        System.out.println("Sum of " + a + " & " + b + " = " + c);
+    }
+}
